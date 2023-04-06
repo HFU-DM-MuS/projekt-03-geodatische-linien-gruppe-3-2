9895c63ddaf497374687b9d9623c63de9b6d6a8f
Niklas <niklasriedinger@hotmail.com>
2023-04-06 23:36:17 +0200

    added Coordinate class

---
+++ src/math/Coordinate.java
@@ -0,0 +1,27 @@
+package math;
+
+import utils.Constants;
+
+public class Coordinate {
+    private final double longitude;
+    private final double latitude;
+
+    private final double r = Constants.GLOBE_SCALE;
+
+    public Coordinate(double lon, double lat) {
+        this.longitude = lon;
+        this.latitude = lat;
+    }
+
+    public Coordinate() {
+        this(0.0, 0.0);
+    }
+
+    public Vector toCartesian() {
+        return new Vector(
+                this.r * Math.cos(Math.toRadians(this.latitude)) * Math.cos(Math.toRadians(this.longitude)),
+                this.r * Math.cos(Math.toRadians(this.latitude)) * Math.sin(Math.toRadians(this.longitude)),
+                this.r * Math.sin(Math.toRadians(this.latitude))
+        );
+    }
+}
